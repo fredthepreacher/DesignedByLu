ed9975fbbea3ba66af0ee775fad92f5dca93eba6
fredthepreacher <fredthepreacher@gmail.com>
2026-06-30 11:02:52 -0400

    v1

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.menu-toggle');
+const links = document.querySelector('.nav-links');
+if (toggle && links) {
+  toggle.addEventListener('click', () => links.classList.toggle('open'));
+}
+document.querySelectorAll('[data-year]').forEach(el => el.textContent = new Date().getFullYear());
